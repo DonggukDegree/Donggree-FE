f9dbce90fb417f02a1265e664c33186880e688c7
SeungyeonHa <0727ha@naver.com>
2026-04-25 16:29:28 +0900

    fix: lint 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+
 import Logo from '@/assets/Icon.svg?react';
 
 function App() {
@@ -12,19 +13,19 @@ function App() {
       <h4 className="text-heading-4 text-coolgray-90">text-heading-4</h4>
       <h5 className="text-heading-5 text-coolgray-90">text-heading-5</h5>
       <h6 className="text-heading-6 text-coolgray-90">text-heading-6</h6>
- 
+
       {/* Body */}
       <p className="text-body-l text-coolgray-90">text-body-l</p>
       <p className="text-body-m text-coolgray-90">text-body-m</p>
       <p className="text-body-s text-coolgray-90">text-body-s</p>
       <p className="text-body-xs text-coolgray-90">text-body-xs</p>
- 
+
       {/* Button */}
       <p className="text-button-xl text-coolgray-90">text-button-xl</p>
       <p className="text-button-l text-coolgray-90">text-button-l</p>
       <p className="text-button-m text-coolgray-90">text-button-m</p>
       <p className="text-button-s text-coolgray-90">text-button-s</p>
- 
+
       {/* Colors */}
       <p className="text-heading-5 text-primary-30 bg-coolgray-90">text-primary-30</p>
       <p className="text-heading-5 text-primary-60">text-primary-60</p>
