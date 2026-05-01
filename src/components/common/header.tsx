05ef2948c04b7e5c5461757d19c906d11f68fa1b
SeungyeonHa <0727ha@naver.com>
2026-05-01 19:50:37 +0900

    [Refactor/#9] 디자인 시스템 수정 - index.css 폰트 사이즈 축소 - 헤더와 푸터 사이즈도 비율 맞춰 축소

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -11,13 +11,13 @@ const NAV_ITEMS = [
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 w-full bg-white text-button-m text-coolgray-90">
-      <div className="flex items-center justify-between py-5 px-20">
+    <header className="sticky top-0 z-50 w-full bg-white text-coolgray-90">
+      <div className="flex items-center justify-between py-2 px-16">
         <div className="flex items-center gap-12">
           <Link to="/">
-            <Logo className="hover:cursor-pointer" />
+            <Logo className="hover:cursor-pointer w-40 h-auto" />
           </Link>
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 text-button-m">
             {NAV_ITEMS.map((item) => (
               <Link key={item.name} to={item.path} className="px-2 py-3 hover:text-primary-60 hover:cursor-pointer">
                 {item.name}
@@ -25,14 +25,14 @@ export default function Header() {
             ))}
           </div>
         </div>
-        <div className="flex items-center gap-8">
+        <div className="flex items-center gap-8 text-body-m">
           {/* TODO: 경로 수정 필요 */}
           <Link to="/" className="flex items-center gap-2 py-3 hover:cursor-pointer">
-            <Headset />
+            <Headset className="w-5 h-5" />
             <span>고객지원</span>
           </Link>
           <Link to="/my-page" className="flex items-center gap-2 hover:cursor-pointer">
-            <UserThumb />
+            <UserThumb className="w-8 h-8" />
             <span>마이페이지</span>
           </Link>
         </div>
