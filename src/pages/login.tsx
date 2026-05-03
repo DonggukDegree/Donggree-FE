86c5c6f3ac80b0cc1bc59940f36af2b8b8d94aea
SeungyeonHa <0727ha@naver.com>
2026-05-03 16:55:41 +0900

    chore: 버튼 타입 설정

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -9,7 +9,7 @@ export default function Login() {
           <Logo className="w-80 h-auto" />
           <p className="text-body-m text-primary-60">가장 쉽고 빠른 나만의 학업 로드맵, 동그리와 함께</p>
         </div>
-        <button className="flex items-center justify-center gap-4 bg-[#FEE500] rounded-md px-20 py-3 cursor-pointer hover:opacity-90">
+        <button type="button" className="flex items-center justify-center gap-4 bg-[#FEE500] rounded-md px-20 py-3 cursor-pointer hover:opacity-90">
           <Kakao className="w-7 h-7" />
           <span className="text-black text-body-l">카카오톡으로 로그인</span>
         </button>
