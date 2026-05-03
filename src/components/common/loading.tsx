4c617eb2401382b20deaace6c591db034ae4941d
SeungyeonHa <0727ha@naver.com>
2026-05-03 16:16:07 +0900

    [Feat/#15] 로딩 및 404 페이지 구현 - 로딩 컴포넌트 구현 - 404 페이지 구현 - 라우터에 404 페이지 경로 설정

---
+++ src/components/common/loading.tsx
@@ -0,0 +1,29 @@
+export default function Loading() {
+  return (
+    <div className="w-full flex flex-col items-center justify-center m-auto gap-20">
+      <svg
+        className="w-20 h-20 animate-spin"
+        viewBox="0 0 80 80"
+        fill="none"
+      >
+        <circle
+          cx="40"
+          cy="40"
+          r="36"
+          stroke="var(--color-primary-30)"
+          strokeWidth="8"
+        />
+        <circle
+          cx="40"
+          cy="40"
+          r="36"
+          stroke="var(--color-primary-60)"
+          strokeWidth="8"
+          strokeLinecap="round"
+          strokeDasharray="80 226"
+        />
+      </svg>
+      <p className="text-heading-2 text-primary-60">Loading...</p>
+    </div>
+  );
+}
