0a4275da32f015a40ebe25143085453026629d4e
SeungyeonHa <0727ha@naver.com>
2026-05-03 16:48:31 +0900

    [Feat/#17] 로그인 페이지 UI 구현 - UI 구현 - 라우팅 최상위로 설정하여 레이아웃 영향 없게 설정

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,19 @@
+import Kakao from '@/assets/kakao.svg?react';
+import Logo from '@/assets/logo.svg?react';
+
 export default function Login() {
-  return <p className="text-heading-3">로그인 페이지</p>;
+  return (
+    <div className="flex items-center justify-center h-screen bg-primary-30">
+      <div className="flex flex-col items-center gap-12 rounded-md bg-white py-20 px-42">
+        <div className="flex flex-col items-center gap-3">
+          <Logo className="w-80 h-auto" />
+          <p className="text-body-m text-primary-60">가장 쉽고 빠른 나만의 학업 로드맵, 동그리와 함께</p>
+        </div>
+        <button className="flex items-center justify-center gap-4 bg-[#FEE500] rounded-md px-20 py-3 cursor-pointer hover:opacity-90">
+          <Kakao className="w-7 h-7" />
+          <span className="text-black text-body-l">카카오톡으로 로그인</span>
+        </button>
+      </div>
+    </div>
+  );
 }
