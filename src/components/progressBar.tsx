2d227d2409f2ed074df32c5ead9bf606fa6ae01e
SeungyeonHa <0727ha@naver.com>
2026-05-07 17:34:08 +0900

    feat: 프로그레스 바에 애니메이션 추가

--- src/components/progressBar.tsx
+++ src/components/progressBar.tsx
@@ -1,9 +1,17 @@
-export default function ProgressBar({ progress }: { progress: number }) {
+interface IProgressBarProps {
+  progress: number;
+  animate?: boolean;
+}
+
+export default function ProgressBar({ progress, animate = true }: IProgressBarProps) {
   const clampedProgress = Math.round(Math.min(100, Math.max(0, progress)));
 
   return (
     <div className="w-150 bg-primary-30 rounded-full h-4">
-      <div className="bg-primary-60 h-4 rounded-full" style={{ width: `${clampedProgress}%` }} />
+      <div
+        className="bg-primary-60 h-4 rounded-full transition-[width] duration-1000 ease-out"
+        style={{ width: animate ? `${clampedProgress}%` : '0%' }}
+      />
     </div>
   );
 }
